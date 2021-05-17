9af8fddcfb5344d912a0b314dcf2d64502e2b205
RChoudharyji <rajat.goliyan@gmail.com>
2021-05-17 09:51:46 +0530

    First Commit

---
+++ src/test/java/cmdproject/CmdProjectDemoTest.java
@@ -0,0 +1,12 @@
+package cmdproject;
+
+import org.testng.annotations.Test;
+
+public class CmdProjectDemoTest {
+
+	@Test
+	public void demo()
+	{
+		System.out.println("It's the demo one");
+	}
+}

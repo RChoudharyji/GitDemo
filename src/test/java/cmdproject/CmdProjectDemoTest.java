55da5099d3a817be70d61f0df9b5c56521017213
RChoudharyji <rajat.goliyan@gmail.com>
2021-05-17 18:23:20 +0530

    new commit

--- src/test/java/cmdproject/CmdProjectDemoTest.java
+++ src/test/java/cmdproject/CmdProjectDemoTest.java
@@ -12,5 +12,12 @@ public class CmdProjectDemoTest {
 		System.out.println("It's the demo three");
 		System.out.println("It's the demo four");
 		System.out.println("It's the demo five");
+		System.out.println("It's the demo six");
+	}
+	
+	@Test
+	public void demonew()
+	{
+		System.out.println("It's new demo");
 	}
 }
